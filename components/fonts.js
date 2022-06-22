69f29ce3316a7c03c3b234ea9b0fecb2b5cec82d
Danyl Boiko <danylboiko@gmail.com>
2022-06-22 10:42:49 +0800

    initial commit

---
+++ components/fonts.js
@@ -0,0 +1,7 @@
+const Fonts = () => (
+  <style jsx global>{`
+    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;1,100;1,200;1,300;1,400&display=swap');
+  `}</style>
+)
+export default Fonts
