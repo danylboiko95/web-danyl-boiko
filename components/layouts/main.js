5b46083244fdf76a010cf2906e0b30139d5e6c9a
Danyl Boiko <danylboiko@gmail.com>
2022-06-22 11:10:05 +0800

    update metadata

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,7 +15,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
+        <meta name="description" content="Danyl boiko homepage" />
         <meta name="author" content="Danyl Boiko" />
         <meta name="author" content="danylboiko" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
@@ -24,11 +24,11 @@ const Main = ({ children, router }) => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@danylboiko" />
         <meta name="twitter:creator" content="@danylboiko" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+        <meta name="twitter:image" content="/images/danylboiko.jpg" />
         <meta property="og:site_name" content="Danyl Boiko" />
         <meta name="og:title" content="Danyl Boiko" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="/images/danylboiko.jpg" />
         <title>Danyl Boiko - Homepage</title>
       </Head>
 
