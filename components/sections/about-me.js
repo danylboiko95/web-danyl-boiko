719034b67e79fa128c752dfb05bde837b5eb3eb1
Danyl Boiko <danylboiko@gmail.com>
2022-07-01 15:35:58 +0800

    text change

--- components/sections/about-me.js
+++ components/sections/about-me.js
@@ -3,7 +3,6 @@ import {
     Box,
 } from '@chakra-ui/react'
 import BoxWrapper from '../box-wrapper';
-import DynamicText from '../dynamic-text/dynamic-text';
 import Paragraph from '../paragraph'
 
 const AboutMe = () => (
@@ -14,12 +13,12 @@ const AboutMe = () => (
         <Paragraph>
             <Box mb={5}>
                 I am an experienced software engineer with 5 years of experience in the IT industry. I started my career
-                in the field of <DynamicText typeSpeed={0} text={['3D graphics (3ds MAX)']} /> and Unreal Engine with C++ basics.
+                in the field of 3D graphics (3ds MAX) and Unreal Engine with C++ basics.
             </Box>
 
             <Box mb={5}>
                 After 2 years in this domain, I decided to dig deeper into programming, and found that creating products is
-                my passion. I worked as a <DynamicText typeSpeed={0} text={['Full-stack developer']} /> (C#, SQL, React/Angular.js) and have completed the migration
+                my passion. I worked as a Full-stack developer (C#, SQL, React/Angular.js) and have completed the migration
                 from Angular.js. to React. I had experience with blockchain technologies. I developed it from
                 scratch to production as a team lead. I worked on high-performance WebAssembly JS charts with 2D/3D
                 WebGL. As a contact point from the development team, continuously supported customer requests and
@@ -27,7 +26,7 @@ const AboutMe = () => (
             </Box>
 
             <Box mb={5}>
-                On a volunteer basis, I gathered and taught a group of <DynamicText typeSpeed={0} text={['15 beginner programmers.']} />
+                On a volunteer basis, I gathered and taught a group of 15 beginner programmers.
                 Mentored the middle developer to the senior level.
                 The upcoming goals are to study DevOps and pass AWS architect certification.
             </Box>
--- components/sections/journey.js
+++ components/sections/journey.js
@@ -19,7 +19,7 @@ const Journey = () => (
                     <BioCompanyAbout>
                         World leaders in predicting and improving ad effectiveness. I was the main front end developer.
                         But there were also tasks related to the backend that I solved.
-                        A lot of work was with the creation of presentable <DynamicText typeSpeed={0} text={['graphs on d3']} />.
+                        A lot of work was with the creation of presentable graphs on d3.
                         I also mentored the middle developer at the same time.
                         After that, he successfully passed the interview to the senior level.
                         <BioCompanyTechologies>
@@ -36,7 +36,7 @@ const Journey = () => (
                 <BioWrapper>
                     <BioCompany>Typescript developer - SciChart, Remote, United Kingdom 🇬🇧</BioCompany>
                     <BioCompanyAbout>
-                        High loaded 2D/3D charts created by <DynamicText typeSpeed={0} text={['WebAssembly. ']} /> Worked in a team of very professional developers.
+                        High loaded 2D/3D charts created by WebAssembly. Worked in a team of very professional developers.
                         The work was related to pulling С++ in javascript. it was a library for the fastest 2D and 3D charts in javascript.
                         But the responsibility didn&apos;t end there. There were sub-projects, such as creating a website with examples of charts,
                         customer support up to the debugging of their code, server-side rendering for SEO optimization.
@@ -56,7 +56,7 @@ const Journey = () => (
                     <BioCompany>Full-stack developer - TEAMinterantional, Ukraine 🇺🇦</BioCompany>
                     <BioCompanyAbout>
                         Had couple of the projects. First project was related to medical insurance in the USA.
-                        Classic full-stack position. From creating front-end component to <DynamicText typeSpeed={0} text={['CRUD in C#.']} />
+                        Classic full-stack position. From creating front-end component to CRUD in C#.
                         Second project was related to improvement of ecology based on blockchain technology with own
                         tokens and work with Metamask. Also was two MVP projects.
 
@@ -76,7 +76,7 @@ const Journey = () => (
                     <BioCompanyAbout>
                         As a 3D&apos;s Max artist I had a huge experience related not only for 3D&apos;s Max
                         but for additional fields.
-                        As a <DynamicText typeSpeed={0} text={['TeamLead, ']} /> Mentor, Project Manager. For part time I was a tutor in 3D max.
+                        As a TeamLead, Mentor, Project Manager. For part time I was a tutor in 3D max.
                         Also had a small experience with Unreal Engine.
                     </BioCompanyAbout>
                 </BioWrapper>
