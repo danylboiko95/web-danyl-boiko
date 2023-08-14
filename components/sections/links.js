e68dc8e246adf9a941be4b4f4098b2df79327970
Danyl Boiko <danyl.boiko_contractor@recordedfuture.com>
2023-08-14 09:07:51 +0200

    update links section

--- components/sections/links.js
+++ components/sections/links.js
@@ -1,66 +1,69 @@
-import { Box, Heading, List, ListItem } from "@chakra-ui/react"
-import { IoDownload, IoLogoGithub, IoLogoInstagram, IoMail } from "react-icons/io5"
+import { Box, Heading, List, ListItem } from "@chakra-ui/react";
+import {
+  IoDownload,
+  IoLogoGithub,
+  IoLogoInstagram,
+  IoLogoLinkedin,
+  IoMail,
+} from "react-icons/io5";
 
 const Links = () => (
-    <>
-        <Heading as="h3" variant="section-title">
-            Links
-        </Heading>
-        <List
-            display='flex'
-            justifyContent={'space-around'}
-            gap={4}
-            flexDirection={{ base: "column", md: "row" }} mb={5} >
-            <ListItem>
-                <a href="mailto:danylboiko@gmail.com">
-                    <Box
-                        alignItems={"center"}
-                        color={'blue'}
-                        display={'flex'}
-                        gap="5px"
-                    >
-                        <IoMail /> danylboiko@gmail.com
-                    </Box>
-                </a>
-            </ListItem>
-            <ListItem>
-                <a target="_blank" href="https://github.com/danylboiko95" rel="noreferrer">
-                    <Box
-                        alignItems={"center"}
-                        color={'blue'}
-                        display={'flex'}
-                        gap="5px"
-                    >
-                        <IoLogoGithub /> danylboiko95
-                    </Box>
-                </a>
-            </ListItem>
-            <ListItem>
-                <a target="_blank" href="https://instagram.com/danyl_boiko" rel="noreferrer">
-                    <Box
-                        alignItems={"center"}
-                        color={'blue'}
-                        display={'flex'}
-                        gap="5px"
-                    >
-                        <IoLogoInstagram /> danyl_boiko
-                    </Box>
-                </a>
-            </ListItem>
-            <ListItem>
-                <a target="_blank" href="./pdf/Danyl_Boiko_CV.pdf">
-                    <Box
-                        alignItems={"center"}
-                        color={'blue'}
-                        display={'flex'}
-                        gap="5px"
-                    >
-                        <IoDownload /> Download CV
-                    </Box>
-                </a>
-            </ListItem>
-        </List>
-    </>
-)
+  <>
+    <Heading as="h3" variant="section-title">
+      Links
+    </Heading>
+    <List
+      display="flex"
+      justifyContent={"space-around"}
+      gap={4}
+      flexDirection={{ base: "column", md: "row" }}
+      mb={5}
+    >
+      <ListItem>
+        <a href="https://www.linkedin.com/in/vizboyko/">
+          <Box alignItems={"center"} color={"blue"} display={"flex"} gap="5px">
+            <IoLogoLinkedin /> LinkedIn
+          </Box>
+        </a>
+      </ListItem>
+      <ListItem>
+        <a href="mailto:danylboiko@gmail.com">
+          <Box alignItems={"center"} color={"blue"} display={"flex"} gap="5px">
+            <IoMail /> danylboiko@gmail.com
+          </Box>
+        </a>
+      </ListItem>
+      <ListItem>
+        <a
+          target="_blank"
+          href="https://github.com/danylboiko95"
+          rel="noreferrer"
+        >
+          <Box alignItems={"center"} color={"blue"} display={"flex"} gap="5px">
+            <IoLogoGithub /> danylboiko95
+          </Box>
+        </a>
+      </ListItem>
+      <ListItem>
+        <a
+          target="_blank"
+          href="https://www.instagram.com/dev_with_boiko/"
+          rel="noreferrer"
+        >
+          <Box alignItems={"center"} color={"blue"} display={"flex"} gap="5px">
+            <IoLogoInstagram /> dev_with_boiko
+          </Box>
+        </a>
+      </ListItem>
+      <ListItem>
+        <a target="_blank" href="./pdf/Danyl_Boiko_CV.pdf">
+          <Box alignItems={"center"} color={"blue"} display={"flex"} gap="5px">
+            <IoDownload /> Download CV
+          </Box>
+        </a>
+      </ListItem>
+    </List>
+  </>
+);
 
-export default Links
+export default Links;
